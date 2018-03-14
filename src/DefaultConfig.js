f076f5001b5bf2cf484632c5c36e921c0550c2ae
Jojo <jm.machado.jm@gmail.com>
2018-03-14 12:20:06 +0000

    fix composer resolution

--- src/DefaultConfig.js
+++ src/DefaultConfig.js
@@ -3,9 +3,13 @@ const config = {
   postPro: true,
   sphereSize:2,
   friction: 0.999,
-  gravity:0.001,
-  forceMultiplier:0.1,
-  drawType:'TRIANGLES'
+  gravity:0.01,
+  forceMultiplier:0.3,
+  drawType:'TRIANGLES',
+  bloom:true,
+  noise:true,
+  // fxaa:true,
+  invert:true,
 }
 
 export default config;
